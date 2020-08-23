682264463c83d53b48ad43024dd53209a92af267
Aspen-The-Deer <62406369+Aspen-The-Deer@users.noreply.github.com>
2020-08-23 03:48:24 +0100

    Github API Interactions

--- cogs/Punish.py
+++ cogs/Punish.py
@@ -3,6 +3,11 @@ import sys
 import time
 import random
 import os
+import github
+import requests
+import base64
+import json
+import datetime
 from discord.ext import commands
 
 class Punishments(commands.Cog):
@@ -180,7 +185,63 @@ class Punishments(commands.Cog):
             with f:
                 f.write(f"User: {user_id}, Warn Location: {guild_id}, Reason: {reasons}")
                 print(str(user_id)+' In ' +str(guild_id)+ ' Has been Warned For: ' +str(reasons))
+                gitHubFileName = ('warns.txt')
+                fileName = ('warns.txt')
+                repo_slug = ('Aspen-The-Deer/Guardian-Deer')
+                branch = ('master')
+                user = ('Aspen-The-Deer')
+                token = (os.environ['GIT_TOKEN'])
+                message = "Automated update " + str(datetime.datetime.now())
+                path = "https://api.github.com/repos/%s/branches/%s" % (repo_slug, branch)
 
+                r = requests.get(path, auth=(user,token))
+                if not r.ok:
+                    print("Error when retrieving branch info from %s" % path)
+                    print("Reason: %s [%d]" % (r.text, r.status_code))
+                rjson = r.json()
+                treeurl = rjson['commit']['commit']['tree']['url']
+                r2 = requests.get(treeurl, auth=(user,token))
+                if not r2.ok:
+                    print("Error when retrieving commit tree from %s" % treeurl)
+                    print("Reason: %s [%d]" % (r2.text, r2.status_code))
+                r2json = r2.json()
+                sha = None
+
+                for file in r2json['tree']:
+                    # Found file, get the sha code
+                    if file['path'] == gitHubFileName:
+                        sha = file['sha']
+
+                # if sha is None after the for loop, we did not find the file name!
+                if sha is None:
+                    print ("Could not find " + gitHubFileName + " in repos 'tree' ")
+                    raise Exception
+
+                with open(fileName) as data:
+                    content = base64.b64encode(data.read())
+
+                # gathered all the data, now let's push
+                inputdata = {}
+                inputdata["path"] = gitHubFileName
+                inputdata["branch"] = branch
+                inputdata["message"] = message
+                inputdata["content"] = content
+                if sha:
+                    inputdata["sha"] = str(sha)
+
+                updateURL = "https://api.github.com/repos/EBISPOT/RDF-platform/contents/" + gitHubFileName
+                try:
+                    rPut = requests.put(updateURL, auth=(user,token), data = json.dumps(inputdata))
+                    if not rPut.ok:
+                        print("Error when pushing to %s" % updateURL)
+                        print("Reason: %s [%d]" % (rPut.text, rPut.status_code))
+                        raise Exception
+                except requests.exceptions.RequestException as e:
+                    print ('Something went wrong! I will print all the information that is available so you can figure out what happend!')
+                    print (rPut)
+                    print (rPut.headers)
+                    print (rPut.text)
+                    print (e)
 
 def setup(client):
     client.add_cog(Punishments(client))

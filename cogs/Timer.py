dd1b3f6de500482b9a2cd0b86851410212786678
Aspen-The-Deer <62406369+Aspen-The-Deer@users.noreply.github.com>
2020-08-31 20:59:35 +0100

    Update Timer.py

--- cogs/Timer.py
+++ cogs/Timer.py
@@ -21,9 +21,13 @@ class Timer(commands.Cog):
         current_time = time.time()
         difference = int(round(current_time - start_time))
         text = str(datetime.timedelta(seconds=difference))
-        embed = discord.Embed(colour=ctx.message.author.top_role.colour)
-        embed.add_field(name="Uptime", value=text)
-        embed.set_footer(text="Sponsored by altcointrain.com - Choo!!! Choo!!!")
+        embed= discord.Embed(
+            colour=(0x629632)
+        )
+
+        embed.set_author(name="Guardian Deer", icon_url="https://cdn.discordapp.com/avatars/606855758612660327/98b13ab2d31342848754caa909a653da.png?size=1024")
+        embed.add_field(name = ('Uptime'), value = (text))
+        embed.set_footer(text="More Features Coming Soon! We're still in Alpha™")
         try:
             await ctx.channel.send(embed=embed)
         except discord.HTTPException:

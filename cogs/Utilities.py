d110dd2f7d6edf10f23bcb65df287267ee7eef9d
Aspen-The-Deer <62406369+Aspen-The-Deer@users.noreply.github.com>
2020-09-02 00:27:55 +0100

    Update Utilities.py

--- cogs/Utilities.py
+++ cogs/Utilities.py
@@ -20,12 +20,13 @@ class Utilities(commands.Cog):
     @commands.command(aliases = ['invite', 'add', 'Add'])
     async def Invite(self, ctx):
         member = ctx.message.author
+        mem = member.Mention
         embed= discord.Embed(
             colour=(0x629632)
         )
 
         embed.set_author(name="Guardian Deer", icon_url="https://cdn.discordapp.com/avatars/606855758612660327/98b13ab2d31342848754caa909a653da.png?size=1024")
-        embed.add_field(name="Invite link sent to:", value=str(member), inline=False)
+        embed.add_field(name="Invite link sent to:", value=str(mem), inline=False)
         embed.set_footer(text="More Features Coming Soon! We're still in Alpha™")
 
         await ctx.channel.send(embed=embed)

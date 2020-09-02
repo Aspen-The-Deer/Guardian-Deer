81bfa716a154a4d6fe582d9e3b439072322bb65e
Aspen-The-Deer <62406369+Aspen-The-Deer@users.noreply.github.com>
2020-09-02 16:40:51 +0100

    Update Logging.py

--- cogs/Logging.py
+++ cogs/Logging.py
@@ -16,13 +16,9 @@ class Logging(commands.Cog):
         print('Logging.py')
 
     @commands.Cog.listener()
-    async def on_message_edit(self, before, after, ctx):
-        global guild
-        guild = ctx.message.guild
-        #member = ctx.message.author
-        #channel = ctx.message.channel
-        #channel = channel
-        logger = discord.utils.get(guild.text_channels, name="logs")
+    async def on_message_edit(self, ctx, before, after):
+        member = after.author.display_name
+        logger = discord.utils.get(ctx.guild.text_channels, name='logs')
         if not after.author.bot:
             if before.content != after.content:
                 embed= discord.Embed(
@@ -30,8 +26,7 @@ class Logging(commands.Cog):
                 )
 
                 embed.set_author(name="Guardian Deer", icon_url="https://cdn.discordapp.com/avatars/606855758612660327/98b13ab2d31342848754caa909a653da.png?size=1024")
-                #embed.add_field(name = ('Message Edited in:'), value = (channel))
-                #embed.add_field(name = ('Author:'), value = (member))
+                embed.add_field(name = ('Author:'), value = (member))
                 embed.add_field(name = ('Before:'), value = (before))
                 embed.add_field(name = ('After:'), value = (after))
                 embed.set_footer(text="More Features Coming Soon! We're still in Alpha™")

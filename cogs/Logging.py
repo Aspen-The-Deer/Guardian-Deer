18df9f6a8b6bbc1923f6e0b3c6ed7bd655b68658
Aspen-The-Deer <62406369+Aspen-The-Deer@users.noreply.github.com>
2020-09-02 02:49:35 +0100

    Update Logging.py

--- cogs/Logging.py
+++ cogs/Logging.py
@@ -14,7 +14,7 @@ class Logging(commands.Cog):
         print('Logging.py')
 
     @commands.Cog.listener()
-    async def on_message_edit(self, ctx, before, after, message, channel: discord.channel=None):
+    async def on_message_edit(self, ctx, before, after, channel: discord.channel=None):
         member = ctx.message.author
         channel = channel
         logger = discord.utils.get(message.server.channels, name="logs")
@@ -26,7 +26,6 @@ class Logging(commands.Cog):
 
                 embed.set_author(name="Guardian Deer", icon_url="https://cdn.discordapp.com/avatars/606855758612660327/98b13ab2d31342848754caa909a653da.png?size=1024")
                 embed.add_field(name = ('Message Edited in:'), value = (channel))
-                embed.add_field(name = ('Author:'), value = (member))
                 embed.add_field(name = ('Before:'), value = (before))
                 embed.add_field(name = ('After:'), value = (after))
                 embed.set_footer(text="More Features Coming Soon! We're still in Alpha™")

2a286f30c8ed416074a6ecce1a6e024bc9be20c3
Aspen-The-Deer <62406369+Aspen-The-Deer@users.noreply.github.com>
2020-09-03 19:12:50 +0100

    Logging Semi-Full functionality Implimentation

--- cogs/Logging.py
+++ cogs/Logging.py
@@ -34,6 +34,23 @@ class Logging(commands.Cog):
                 embed.set_footer(text="More Features Coming Soon! We're still in Alpha™")
                 await logger.send(embed=embed)
 
+    @commands.Cog.listener()
+    async def on_message_delete(self, message):
+        member = message.author.mention
+        logger = discord.utils.get(message.guild.channels, name='logs')
+        location = message.channel.mention
+        if not message.author.bot:
+            embed= discord.Embed(
+                colour=(0x629632)
+            )
+
+            embed.set_author(name="Guardian Deer", icon_url="https://cdn.discordapp.com/avatars/606855758612660327/98b13ab2d31342848754caa909a653da.png?size=1024")
+            embed.add_field(name = ('Author:'), value = (member), inline=False)
+            embed.add_field(name = ('Message Content:'), value = ('"')+(message.content)+('"'), inline=False)
+            embed.add_field(name = ('In:'), value = (location), inline=False)
+            embed.set_footer(text="More Features Coming Soon! We're still in Alpha™")
+            await logger.send(embed=embed)
+
 
 def setup(client):
     client.add_cog(Logging(client))
